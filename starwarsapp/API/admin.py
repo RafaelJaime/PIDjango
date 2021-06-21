1becf21b3f3028ca1568a0f9233ffc91731875c0
RafaelJaimeMoreno <rajaimor@gmail.com>
2021-06-21 04:24:38 +0200

    comment

--- starwarsapp/API/admin.py
+++ starwarsapp/API/admin.py
@@ -6,13 +6,13 @@ from .models import Article, Event, Post, Answer, Product, Film, FilmCommentary,
 # Tabbar
 # Pantalla1
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'event')
-    search_fields = ['title', 'author']
-    list_filter = ['author']
+    # list_display = ('id', 'title', 'author', 'event')
+    # search_fields = ['title', 'author']
+    # list_filter = ['author']
 admin.site.register(Article, ArticleAdmin)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'telephone', 'email', 'startDate')
-    search_fields = ['title', 'description']
+    # list_display = ('id', 'title', 'telephone', 'email', 'startDate')
+    # search_fields = ['title', 'description']
 admin.site.register(Event, EventAdmin)
 
 # Pantalla2
